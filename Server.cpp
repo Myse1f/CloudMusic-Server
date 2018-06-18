71e72cb5a4950fd7bd2d00c298612e77bdea7f8b
Myse1f <602149025@qq.com>
2018-06-19 00:09:17 +0800

    update

--- Server.cpp
+++ Server.cpp
@@ -1,13 +1,21 @@
 #include "Server.h"
 #include <QtDebug>
-#include <iostream>
+//#include <iostream>
+
+#define PORT 10000
+
 Server::Server(QObject *parent) : QTcpServer(parent) {
 	qDebug() << "Server";
 }
 
 void Server::incomingConnection(qintptr socketDescriptor) {
-	std::cout << "new connection" << endl;
+	qDebug() << "new connection";
     UserThread *thread = new UserThread(socketDescriptor, this);
     connect(thread, SIGNAL(finished()), thread, SLOT(deleteLater()));
     thread->start();
+}
+
+void Server::run() {
+	this->listen(QHostAddress::Any, PORT);
+	//exec();
 }
